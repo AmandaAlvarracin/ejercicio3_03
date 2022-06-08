75af1e0b1dd24f06bd18938363f8d44a1bf49961
AmandaAlvarracin <103578858+AmandaAlvarracin@users.noreply.github.com>
2022-06-08 14:01:22 -0500

    Update Articulo.java

--- src/main/java/modelo/Articulo.java
+++ src/main/java/modelo/Articulo.java
@@ -9,22 +9,27 @@ package modelo;
  * @author User
  */
 public class Articulo {
-    
-   String titulo;
-   int numeroPalabras;
-   localDate fechaPublicacion;
+    String titulo;
+    int numeroPalabras; //maximo 
+    int limitePalabras;
+    int yearPublicacion;
     Revista revista;
     AutorPrincipal autor;
-    public int calcularNumeroPalab(){
-        
-        
-     
-    public boolean verificarFecha(){
-        
-        
-       return false;
-        
-        
-     }
+    public boolean verificarNumeroPalabras(){ 
+        var retorno=false;
+        if(this.numeroPalabras>=0 && this.numeroPalabras<=limitePalabras){
+                retorno=true; 
+        }
+        return retorno ;   
+    }
+    public String verificarFecha(){
+     var retorno= " ";
+      if (this.yearPublicacion>=2015 && this.yearPublicacion<= 2022){  
+          retorno= " Publicacion Actual"; 
+      }else if (this.yearPublicacion>=2010 && this.yearPublicacion<=2015){
+          retorno= "Publicacion Antigua";
+      }
+        return retorno;   
+    }   
+  
 }
-        
